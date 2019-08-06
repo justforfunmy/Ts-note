af406415505e3c37346ae0d4915a4ff6c3614efa
tangkaiqiang <justforgit@163.com>
2019-08-06 10:45:35 +0800

    set

--- examples/react-ts-demo/src/views/dataStructure/index.tsx
+++ examples/react-ts-demo/src/views/dataStructure/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BinaryTree from '../../entities/binaryTree/BinaryTree'
 import Stack from '../../entities/stack'
 import LinkedList from '../../entities/linkedList'
+import Set from '../../entities/set'
 
 export default class PageDataStructure extends React.Component{
     handleBinary=()=>{
@@ -40,13 +41,65 @@ export default class PageDataStructure extends React.Component{
         linkedList.append(123)
         console.log(linkedList)
     }
+    handleUnionSet=()=>{
+        let set = new Set();
+        set.add(2);
+        set.add(5);
+        let otherSet = new Set();
+        otherSet.add(3);
+        console.log(set.union(otherSet))
+    }
+    handleIntersectionSet=()=>{
+        let set = new Set();
+        set.add(2);
+        set.add(5);
+        let otherSet = new Set();
+        otherSet.add(3);
+        otherSet.add("5");
+        console.log(set.intersection(otherSet))
+    }
+    handleSubset=()=>{
+        let setA = new Set();
+        setA.add(1);
+        setA.add(2);
+        let setB = new Set();
+        setB.add(1);
+        setB.add(2);
+        setB.add(3);
+        let setC = new Set();
+        setC.add(2);
+        setC.add(3);
+        setC.add(4);
+        console.log(setA.subset(setB));
+        console.log(setA.subset(setC));
+    }
+    handleDiffence=()=>{
+        let setA = new Set();
+        setA.add(1);
+        setA.add(2);
+        setA.add(3);
+        let setB = new Set();
+        setB.add(2);
+        setB.add(3);
+        setB.add(4);
+        let differenceAB = setA.difference(setB);
+        console.log(differenceAB.values());
+    }
     render(){
         return (
             <div>
+                <h3>二叉树</h3>
                 <button onClick={this.handleBinary}>binary</button>
+                <h3>栈</h3>
                 <button onClick={this.handleStack}>stack</button>
-                <div>{this.handleDivideBy2(125)}</div>
+                <div>十进制转二进制：{this.handleDivideBy2(125)}</div>
+                <h3>链表</h3>
                 <button onClick={this.handleLinkedList}>linkedList</button>
+                <h3>集合</h3>
+                <button onClick={this.handleUnionSet}>并集</button>
+                <button onClick={this.handleIntersectionSet}>交集</button>
+                <button onClick={this.handleSubset}>子集</button>
+                <button onClick={this.handleDiffence}>差集</button>
             </div>
         )
     }
