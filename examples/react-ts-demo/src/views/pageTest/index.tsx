60f2418bd5a639f6ebbbc70f2dd52ce557a9134c
tangkaiqiang <justforgit@163.com>
2019-08-14 17:33:41 +0800

    fn

--- examples/react-ts-demo/src/views/pageTest/index.tsx
+++ examples/react-ts-demo/src/views/pageTest/index.tsx
@@ -12,6 +12,13 @@ export default class PageTest extends React.Component {
         console.log(newArr)
         return newArr
     }
+    testFunction=()=>{
+        let fn=function f(params:number) {
+            console.log(f)
+        }
+        console.log(typeof fn)
+        fn(1)
+    }
     componentDidMount(){
         // let obj= new Object()
         // console.log(this.testVar(obj));
@@ -22,14 +29,21 @@ export default class PageTest extends React.Component {
         // let obj1={name:'123'};
         // let obj2=obj1;
         // obj2.name='1234';
+        // obj2=null as any;
+
         // console.log(obj1,obj2)
 
-        let colors = [{text:'red'},{text:'green'}]
-        console.log(colors.toLocaleString(),colors.toString(),colors.values())
-        let nums = [1,2];
-        let nums2 = nums.concat();
-        nums2.splice(1,1);
-        console.log(nums,nums2)
+        // let colors = [{text:'red'},{text:'green'}]
+        // console.log(colors.toLocaleString(),colors.toString(),colors.values())
+        // let nums = [1,2];
+        // let nums2 = nums.concat();
+        // nums2.splice(1,1);
+        // console.log(nums,nums2)
+
+        // let date = new Date();
+        // console.log(date.toString(),date.toDateString(),date.toTimeString());
+
+        this.testFunction()
     }
     render(){
         return (
