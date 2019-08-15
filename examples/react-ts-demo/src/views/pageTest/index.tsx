9084a91c3d6a2fafaee35a45a60a849e57379a46
tangkaiqiang <justforgit@163.com>
2019-08-15 14:41:30 +0800

    1

--- examples/react-ts-demo/src/views/pageTest/index.tsx
+++ examples/react-ts-demo/src/views/pageTest/index.tsx
@@ -19,6 +19,43 @@ export default class PageTest extends React.Component {
         console.log(typeof fn)
         fn(1)
     }
+    /* 工厂模式虽然解决了创建
+    多个相似对象的问题，但却没有解决对象识别的问题（即怎样知道一个对象的类型） */
+    factortyMode=()=>{
+        function createPerson(name:string) {
+            let o:any = {};
+            o.name = name;
+            return o;
+        }
+        let person = createPerson('king');
+        console.log(person)
+    }
+    /* 要创建 Person 的新实例，必须使用 new 操作符。以这种方式调用构造函数实际上会经历以下 4
+    个步骤：
+    (1) 创建一个新对象；
+    (2) 将构造函数的作用域赋给新对象（因此 this 就指向了这个新对象）；
+    (3) 执行构造函数中的代码（为这个新对象添加属性）；
+    (4) 返回新对象。 */
+    constructorMode=()=>{
+        // function Person(name:string) {
+        //     this.name = name;
+        // }
+        // let person = new Person('king')
+        // console.log(person)
+    }
+    /* 原型模式
+    每个函数都有一个 prototype（原型）属性，这个属性是一个指针，指向一个对象，
+    而这个对象的用途是包含可以由特定类型的所有实例共享的属性和方法。 */
+    protoMode=()=>{
+        /* Person.prototype. constructor 指向 Person。 */
+        function Person() {
+            
+        }
+        Person.prototype.name='king';
+        // let person = new Person();
+        // console.log(person)
+
+    }
     componentDidMount(){
         // let obj= new Object()
         // console.log(this.testVar(obj));
@@ -47,7 +84,10 @@ export default class PageTest extends React.Component {
     }
     render(){
         return (
-            <div>PageTest</div>
+            <div>
+                <h3>设计模式</h3>
+                <button onClick={this.factortyMode}>工厂模式</button>
+            </div>
         )
     }
 }
